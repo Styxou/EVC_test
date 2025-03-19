a3ce5ab9c6fba667b816796fbe35736bf519e88d
W4ldschr31n <nicolasrich@hotmail.fr>
2025-03-19 18:07:05 +0100

    [ADD] tests

---
+++ Assets/RICHARD_Nicolas/WinCube.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+
+public class WinCube : MonoBehaviour
+{
+    public Transform nextCheckpoint;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerMovement player = other.transform.parent.GetComponent<PlayerMovement>();
+        Debug.Log("allo"+player);
+        if (player != null)
+        {
+            other.transform.parent.position = nextCheckpoint.position;
+        }
+    }
+}
