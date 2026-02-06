98283518c66cd33e014cb22c3ba3e70d8614bb1e
Xorrell <85642232+Xorrell@users.noreply.github.com>
2026-02-06 09:29:57 +0100

    working cork walls + more of the stage

--- Assets/Core/Script/Character/Grappling.cs
+++ Assets/Core/Script/Character/Grappling.cs
@@ -8,7 +8,7 @@ public class Grappling : MonoBehaviour
     private PlayerMovement pm;
     public Transform cam;
     public Transform gunTip;
-    public LayerMask whatIsGrappleable;
+    public LayerMask whatIsGrappleable; 
 
     [Header("Grappling")]
     public float maxGrappleDistance;
@@ -85,7 +85,7 @@ public class Grappling : MonoBehaviour
 
             if (Physics.Raycast(cam.position, cam.forward, out hit, maxGrappleDistance))
             {
-                if (hit.transform.gameObject.layer == 7)
+                if (hit.transform.gameObject.layer == 7 || hit.transform.gameObject.layer == 13)
                 {
                     grapplePoint = hit.point;
 
