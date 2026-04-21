6cab8eab373cc280ed6df7210c5d525e30af97a9
Styxou <59559495+Styxou@users.noreply.github.com>
2026-04-21 13:09:40 +0200

    script

--- Assets/ATELIER/Piece.cs
+++ Assets/ATELIER/Piece.cs
@@ -2,11 +2,24 @@ using UnityEngine;
 
 namespace Athena.Prototype
 {
+    
+
     public class UI : MonoBehaviour
     {
+        [SerializeField] GameObject g_piece;
+        Score score;
         void Start() 
         {
+            score = GameObject.Find("Player").gameObject.GetComponent<Score>();
         }
-       
+
+        private void OnTriggerEnter(Collider other)
+        {
+            score.addScore();
+            Destroy(g_piece.gameObject);
+        }
+
     }
+
+
 }
